0467c8dcca785a633e4b6a2e910421fb05a82271
Jeff Retz <jeff.retz08@gmail.com>
2026-05-25 21:07:08 +0200

    prepare ui

--- frontend/src/nativeMain/kotlin/dev/datlag/isekai/Main.kt
+++ frontend/src/nativeMain/kotlin/dev/datlag/isekai/Main.kt
@@ -1,36 +1,56 @@
 package dev.datlag.isekai
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import dev.datlag.isekai.ipc.ConnectionState
-import dev.datlag.isekai.ipc.DiskManagerRepository
-import dev.datlag.isekai.ipc.IpcRequest
 import dev.datlag.isekai.ipc.IpcTransport
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
+import dev.datlag.kommons.adwaita.compose.adwaitaApplication
+import dev.datlag.kommons.adwaita.compose.component.Scaffold
+import dev.datlag.kommons.adwaita.compose.component.StatusPage
+import dev.datlag.kommons.adwaita.compose.component.TopAppBar
+import dev.datlag.kommons.adwaita.compose.component.WindowTitle
+import dev.datlag.kommons.gtk.compose.modifier.Modifier
+import dev.datlag.kommons.gtk.compose.modifier.fillMaxSize
+import dev.datlag.kommons.gtk.compose.modifier.fillMaxWidth
 
-@OptIn(ExperimentalUuidApi::class)
-fun main() = runBlocking {
-    val client = IpcTransport()
+fun main(args: Array<String>) = adwaitaApplication(
+    applicationId = "dev.datlag.isekai",
+    title = "Isekai",
+    args = args.asIterable()
+) {
+    val ipcClient = remember { IpcTransport() }
 
-    launch {
-        client.connectionState.collect { state ->
-            when (state) {
-                is ConnectionState.Connecting -> println("Isekai Frontend connecting...")
-                is ConnectionState.Connected -> {
-                    val repo = DiskManagerRepository(client)
-
-                    println(repo.checkSystem().getOrNull())
-                }
-                is ConnectionState.Error -> {
-                    println(state.message)
-                    state.exception?.printStackTrace()
-                }
-                is ConnectionState.Disconnected -> println("Isekai Frontend disconnected...")
-            }
-        }
+    LaunchedEffect(ipcClient) {
+        ipcClient.connect()
     }
 
-    client.connect()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = { WindowTitle("Isekai") },
+            )
+        }
+    ) {
+        val connectionState by ipcClient.connectionState.collectAsState()
+
+        StatusPage(
+            modifier = Modifier.fillMaxSize(),
+            icon = when (connectionState) {
+                is ConnectionState.Connecting -> Symbols.NETWORK_CONNECTING
+                is ConnectionState.Connected -> Symbols.NETWORK_CONNECTED
+                is ConnectionState.Disconnected -> Symbols.NETWORK_DISCONNECTED
+                is ConnectionState.Error -> Symbols.NETWORK_ERROR
+            },
+            title = when (connectionState) {
+                is ConnectionState.Connecting -> "Connecting"
+                is ConnectionState.Connected -> "Connected"
+                is ConnectionState.Disconnected -> "Disconnected"
+                is ConnectionState.Error -> "Connection Error"
+            }
+        )
+    }
 }
---
+++ frontend/src/nativeMain/kotlin/dev/datlag/isekai/Symbols.kt
@@ -0,0 +1,14 @@
+package dev.datlag.isekai
+
+import dev.datlag.kommons.gtk.compose.component.IconName
+
+object Symbols {
+
+    val NETWORK_CONNECTING = IconName("network-wired-acquiring-symbolic")
+    val NETWORK_CONNECTED = IconName("network-wired-symbolic")
+    val NETWORK_DISCONNECTED = IconName("network-wired-disconnected-symbolic")
+    val NETWORK_ERROR = IconName("network-wired-no-route-symbolic")
+
+    val PACKAGES = IconName("system-software-install-symbolic")
+
+}
